6229842663f525b8e2e50af4da66e49132f313ef
sws92014 <sws92014@gmail.com>
2016-06-22 19:46:44 -0500

    made major changes to css, js, and index

--- js/app.js
+++ js/app.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-    $('.sf-logo').show ("slow", function(){
-        $('.sf-logo').fadeIn ("9500", function(){
-         $(this).fadeOut ("5000", function(){
-            $('.sf-logo').hide();
-                $('.brought-by').show();
-                    $('.brought-by').fadeIn ("9500", function(){
-                    $(this).fadeOut ("5000", function(){
-                        $('.brought-by').hide();
-                });
-            });
-    });  
 
     $('.ryu_figure').mouseenter(function(){
         $('.ryu_still').hide();
@@ -22,6 +11,8 @@ $(document).ready(function(){
     .mousedown(function(){
         playHadouken();
         $('.ryu_ready').hide();
+        $('ryu_cool').hide();
+        $('ryu_still').hide();
         $('.ryu_throwing').show();
         $('.hadouken').finish().show().animate(
             {'left': '1020px'},
@@ -33,19 +24,26 @@ $(document).ready(function(){
     })
     .mouseup(function(){
         $('.ryu_throwing').hide();
+        $('ryu_cool').hide();
+        $('ryu_still').hide();
         $('.ryu_ready').show();
     });
     $('body').keydown(function(event){
       if (event.keyCode == 88) {
         $('.ryu_still').hide();
+        $('ryu_ready').hide();
         $('.ryu_cool').show();
+        $('.hadouken').hide();
       }
     })
-    .keyup(function(){
-        $('.ryu_cool').hide();
-        $('.ryu_ready').show();
-        $('.ryu_still').show();
-    })
+    .keyup(function(e) {
+    if (e.keyCode == 88) {
+      $('.ryu_cool').hide();
+      $('.ryu_still').show();
+      $('ryu_ready').hide();
+      $('.hadouken').hide();
+    }
+  })
 });
 
 function playHadouken() {
@@ -54,3 +52,4 @@ function playHadouken() {
     $('#hadouken-sound')[0].play();
 
 }
+
